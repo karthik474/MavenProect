61ed53b4cd41f589bbfbcd2d0945647c06fac5d9
karthik474 <80387293+karthik474@users.noreply.github.com>
2025-11-01 10:41:28 +0000

    add

---
+++ read.md
@@ -0,0 +1,7 @@
+3. Key Differences
+Feature	List	Dictionary
+Structure	Ordered collection of items	Key-value pairs
+Access	By index (0, 1, 2…)	By key
+Syntax	[item1, item2, ...]	{key1: value1, key2: value2}
+Duplicate	Allowed	Keys must be unique
+Use case	Sequence of items	Mapping of keys to values
--- test.py
+++ test.py
@@ -154,9 +154,11 @@
 # else:
 #     print("not found 1")
 #==================
-numbers = [1, 2, 3, 4, 5,]
-for num in numbers:
-    if num == 3:
-        print(num)
+# numbers = [1, 2, 3, 4, 5,]
+# for num in numbers:
+#     if num == 3:
+#         print(num)
         # continue
     # print(num)
+#==============================
+  
